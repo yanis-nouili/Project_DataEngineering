c7ad2aae05815ac391041b9913ff908e665dbb99
yacine.oualiken <yacine.oualiken@edu.esiee.fr>
2026-02-11 19:22:40 +0100

    Add files via upload

--- db/init.sql
+++ db/init.sql
@@ -42,3 +42,40 @@ CREATE TABLE IF NOT EXISTS scorers (
     scraped_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     UNIQUE (season, player_name)
 );
+
+-- ======================
+-- PALMARES CLUBS
+-- ======================
+CREATE TABLE IF NOT EXISTS palmares_clubs (
+    id SERIAL PRIMARY KEY,
+    team TEXT,
+    titles INTEGER,
+    scraped_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    UNIQUE(team)
+);
+
+-- ======================
+-- HISTORIQUE TITRES
+-- ======================
+CREATE TABLE IF NOT EXISTS palmares_history (
+    id SERIAL PRIMARY KEY,
+    season VARCHAR(20),
+    winner TEXT,
+    runner_up TEXT,
+    scraped_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    UNIQUE(season)
+);
+
+-- Table des passeurs (qui manquait apparemment)
+CREATE TABLE IF NOT EXISTS assists (
+    id SERIAL PRIMARY KEY,
+    season VARCHAR(20),
+    rank INTEGER,
+    player_name VARCHAR(100),
+    team VARCHAR(100),
+    assists INTEGER,
+    scraped_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    UNIQUE (season, player_name, team)
+);
+
+ALTER TABLE standings ADD COLUMN IF NOT EXISTS logo_url TEXT;
